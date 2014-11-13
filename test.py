5a17188eaaebe2dc25996a51f7eed25e5aa8f64d
renchi <chitoborabien@gmail.com>
2014-11-13 14:33:54 +0800

    updated path

--- test.py
+++ test.py
@@ -2,11 +2,16 @@ import test_util
 import os
 import shutil
 
-currentPath = "C:\jenkins\copyfolders\swversion"
-srcPath = test_util.latestDir( currentPath )
-print ('srcPath ' + srcPath)
+def main():
+    currentPath = os.getcwd() + "/workdir/swversion"
+    srcPath = test_util.latestDir( currentPath )
+    print ('srcPath ' + srcPath)
 
-dstPath = "C:\jenkins\copyfolders\destFolder"
-print( 'destPath =' + dstPath)
+    dstPath = os.getcwd() + "/workdir/destfolder"
+    print( 'destPath =' + dstPath)
 
-autotest.del_and_copy(srcPath, dstPath)
+    test_util.del_and_copy(srcPath, dstPath)
+
+
+if __name__ == '__main__':
+  main()
