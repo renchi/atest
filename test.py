fbe30b222775fd7537cb85970a5a21b2df6413d2
renchi <chitoborabien@gmail.com>
2014-11-13 15:23:14 +0800

    corrected path

--- test.py
+++ test.py
@@ -1,19 +1,31 @@
 import test_util
 import os
+import sys
 import shutil
+
+INSTALL_Sw = ""
+INSTALL_LatestSW = ""
 
 def main():
     SCRIPTPath = os.getcwd()
-    currentPath = os.getcwd() + "/workdir/swversion"
-    srcPath = test_util.latestDir( currentPath )
+
+    srcPath = INSTALL_Sw
+    if INSTALL_Sw == "Bugfix_1" and INSTALL_LatestSW == "Yes":
+        currentPath = os.getcwd() + "/workdir/swversion"
+        srcPath = test_util.latestDir( currentPath )
+
     print ('srcPath ' + srcPath)
 
     os.chdir(SCRIPTPath)
     dstPath = os.getcwd() + "/workdir/destfolder"
     print( 'destPath =' + dstPath)
 
-    test_util.del_and_copy(srcPath, dstPath)
+    #test_util.del_and_copy(srcPath, dstPath)
 
 
 if __name__ == '__main__':
-  main()
+    INSTALL_Sw =  sys.argv[1]
+    INSTALL_LatestSW = sys.argv[2]
+    print "INSTALLSW = " + INSTALL_Sw
+    print "INSTALL_LatestSW = " + INSTALL_LatestSW
+    main()
