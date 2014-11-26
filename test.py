a3ecc4e52bca245bb2fabf58ed4b9d145cbcada0
renchi <chitoborabien@gmail.com>
2014-11-26 11:28:59 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,25 +7,18 @@ INSTALL_Sw = ""
 INSTALL_LatestSW = ""
 
 def main():
-    SCRIPTPath = os.getcwd()
-
-    srcPath = INSTALL_Sw
-    if INSTALL_Sw == "Bugfix_1" and INSTALL_LatestSW == "Yes":
+    dstPath = os.getcwd() + "/workdir/destfolder"
+    if INSTALL_Sw == "Bugfix_1"":
         currentPath = os.getcwd() + "/workdir/swversion"
         srcPath = test_util.latestDir( currentPath )
-
-    print ('srcPath ' + srcPath)
-
-    os.chdir(SCRIPTPath)
-    dstPath = os.getcwd() + "/workdir/destfolder"
-    print( 'destPath =' + dstPath)
-
-    test_util.del_and_copy(srcPath, dstPath)
-
+        test_util.del_and_copy(srcPath, dstPath)
+    else:
+        srcPath = INSTALL_Sw
+        os.path.dirname(srcPath)
+        test_util.del_and_copy(srcPath, dstPath)
 
 if __name__ == '__main__':
-    INSTALL_Sw =  sys.argv[1]
-    INSTALL_LatestSW = sys.argv[2]
-    print "INSTALLSW = " + INSTALL_Sw
-    print "INSTALL_LatestSW = " + INSTALL_LatestSW
+    INSTALL_Sw =  sys.argv[1] 
+    report( "INSTALLSW = " + INSTALL_Sw )
+
     main()
